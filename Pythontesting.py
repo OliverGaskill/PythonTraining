0e52ccfc33ebac7df67663c5da25a1eb96e12896
OliverGaskill <gaskilloliver@gmail.com>
2025-07-09 16:33:28 +0200

    having fun

--- Pythontesting.py
+++ Pythontesting.py
@@ -2,7 +2,7 @@
 #last_name = "Gaskill"
 #full_name = first_name +" "+ last_name
 #print("Hello "+full_name)
-
+from codecs import namereplace_errors
 
 #age = 24
 #age = age + 1
@@ -16,3 +16,21 @@
 #Human = True
 #print("Are you a human: "+str(Human))
 #print(type(Human))
+
+#name = "Oliver"
+#age = 24
+
+#name, age,  = "Oliver", 24
+#print(name)
+#print(age)
+
+#Spongebob = 30
+#Patrick = 30
+#Sandy = 30
+#Squidward = 30
+
+#Spongebob = Patrick = Sandy = Squidward = 30
+#print(Spongebob)
+#print(Patrick)
+#print(Sandy)
+#print(Squidward)
