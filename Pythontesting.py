56601466b9a59e2773923f33fe30ff642231ee71
OliverGaskill <gaskilloliver@gmail.com>
2025-07-09 16:21:55 +0200

    having fun

--- Pythontesting.py
+++ Pythontesting.py
@@ -1,3 +1,18 @@
-name = "Oliver"
-print(type(name))
-#print("Hello "+name)
+#first_name = "Oliver"
+#last_name = "Gaskill"
+#full_name = first_name +" "+ last_name
+#print("Hello "+full_name)
+from sys import flags
+
+#age = 24
+#age = age + 1
+#print("Your age is : "+str(age))
+#print(type(age))
+
+#height = 183.5
+#print("Your height is: "+str(height) +"cm")
+#print(type(height))
+
+#Human = True
+#print("Are you a human: "+str(Human))
+#print(type(Human))
