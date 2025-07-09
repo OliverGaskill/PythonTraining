f34ffe8f1cbd6da1d16a44970dc461805f4add26
OliverGaskill <gaskilloliver@gmail.com>
2025-07-09 17:08:21 +0200

    having fun

--- Pythontesting.py
+++ Pythontesting.py
@@ -59,3 +59,12 @@
 #print("x is "+x)
 #print("y is "+str(y))
 #print(z*3)
+
+#User Input
+#name = input("What is your name?:")
+#age = int(input("How old are you?: "))
+#height = float(input("How tall are you?: "))
+
+#print("Hello "+name)
+#print("You are "+str(age)+" years old")
+#print("You are "+str(height)+"cm tall")
